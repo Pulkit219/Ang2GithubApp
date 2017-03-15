7a7459beb8f80fab61cd2d165affb2906317643c
Pulkit219 <ascension1110@gmail.com>
2017-03-15 11:32:09 -0400

    4th commit

--- Ang2GithubApp/src/app/components/github/github.component.ts
+++ Ang2GithubApp/src/app/components/github/github.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GithubComponent implements OnInit {
 
-  constructor() { }
+  constructor() { console.log("github comp started ")}
 
   ngOnInit() {
   }
